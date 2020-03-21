64b8ccec23f8301c685c5e85966833692b7148be
rus19023 <rus19023@byui.edu>
2020-03-20 20:56:37 -0400

    L9 final

--- lesson-9/js/towns.js
+++ lesson-9/js/towns.js
@@ -1,14 +1,5 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-function sortByValue(jsObj){
-  var sortedArray = [];
-  for(var i in jsObj)
-  {
-      // Push each JSON Object entry in array by [value, key]
-      sortedArray.push([jsObj[i], i]);
-  }
-  return sortedArray.sort();
-};
 
 fetch(requestURL)
   .then(function (response) {
@@ -16,36 +7,25 @@ fetch(requestURL)
   })
   .then(function (jsonObject) { 
     const towns = jsonObject['towns'];
-
-
-var sortedbyValueJSONArray = sortByValue(towns);
-console.table(sortedbyValueJSONArray);
     towns.forEach(town => {
-      if (town.name=="Preston"||town.name=="Soda Springs"||town.name=="Fish Haven") {
-
-
-        
-        
+     
         let card = document.createElement('section'); 
         let div1 = document.createElement('div')
         let div2 = document.createElement('div')
         let h2 = document.createElement('h2');
-        //let town_a = document.createElement('a')
         let h6 = document.createElement('h6');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
         let p3 = document.createElement('p');
         let image = document.createElement('img');
         let town_name = `${town.name}`;
-        //let town_link = `https://rus19023.github.io/lesson-11/${town.name}.html`;
         let imgURL = `images/${town.photo}`;
-        let motto = `${town.motto}` ;
+        let motto = `${town.motto}` 
         let year = `Year founded: ${town.yearFounded}`;
         let population = `Population: ${town.currentPopulation}`;
         let rainfall = `Annual Rainfall: ${town.averageRainfall}`;
 
         h2.textContent = town_name;
-        //town_a.setAttribute('href', town_link);
         image.setAttribute('src', imgURL);  
         image.setAttribute('alt', town_name);
         h6.textContent = motto;
@@ -54,8 +34,7 @@ console.table(sortedbyValueJSONArray);
         p3.textContent =rainfall;
         
         //  build the html code for the cards
-        //h2.appendChild(town_a);  //  add the town name heading to the text div
-        div1.appendChild(h2);
+        div1.appendChild(h2);  //  add the town name heading to the text div
         div1.appendChild(h6);  //  add the motto heading to the text div
         div1.appendChild(p1);  //  add the founding year to the text div
         div1.appendChild(p2);  //  add the population to the text div
@@ -64,15 +43,16 @@ console.table(sortedbyValueJSONArray);
         card.appendChild(div1);  // add the text div to the card div
         card.appendChild(div2);  //  add the image div to the card div
 
-        // set up cards for each town in order of menu
-
-        if (town.name == "Preston") {
-          document.querySelector('#preston').appendChild(card);  //  set the Preston div into the html page
-        } else if (town_name == "Soda Springs") {
-        document.querySelector('#sodasprings').appendChild(card);  //  set the card div into the html page
-        } else if (town_name == "Fish Haven") {
-        document.querySelector('#fishhaven').appendChild(card);  //  set the card div into the html page
+        if (town_name == "Preston") { 
+          document.querySelector('.preston').appendChild(card);  //  set the card div into the html page
         }
-    }
+        if (town_name == "Soda Springs") { 
+          document.querySelector('.sodasprings').appendChild(card);  //  set the card div into the html page
+        }
+        if (town_name == "Fish Haven") { 
+          document.querySelector('.fishhaven').appendChild(card);  //  set the card div into the html page
+        }
+
+    
     });
   });
