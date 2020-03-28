fad46a501e0d0fc41342b9325223ba5bf0598a5e
rus19023 <rus19023@byui.edu>
2020-03-27 20:03:05 -0400

    defer js youtube

--- lesson-11/js/lazyload.js
+++ lesson-11/js/lazyload.js
@@ -31,3 +31,11 @@ if ('IntersectionObserver' in window) {
     });
 }
 
+function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i=0; i<vidDefer.length; i++) {
+    if(vidDefer[i].getAttribute('data-src')) {
+    vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+    } } }
+    window.onload = init;
+
